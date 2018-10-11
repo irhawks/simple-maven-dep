9bc77ddc08f7d98ad57abec837bfff2a7f514616
irhawks <irhawks@163.com>
2018-10-11 21:28:23 +0800

    update

--- src/main/java/com/example/dep/Dep.java
+++ src/main/java/com/example/dep/Dep.java
@@ -4,10 +4,9 @@ package com.example.dep;
  * Hello name!
  *
  */
-public class Dep
-{
-    public static void main( String name )
+public class Dep {
+    public static void main( String[] args )
     {
-        System.out.println( "Hello " + name + "!" );
+        System.out.println( "Hello!" );
     }
 }
